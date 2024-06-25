9c328c17e1f8df3f3c612d36899a03d70d74502c
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-06-25 09:40:01 -0500

    Add sass support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://samfeldstein.xyz',
+    vite: {
+        css: {
+          transformer: "sass",
+        },
+      },
 });
