cc7a2c96dbb273644d4cdaece9a5618e1ae13cb5
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-06-30 08:55:33 -0500

    Prefetch all links by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://samfeldstein.xyz',
-    prefetch: true,
+    prefetch: {
+      prefetchAll: true
+    },
     vite: {
         css: {
           transformer: "sass",
