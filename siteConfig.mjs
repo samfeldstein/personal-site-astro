71e969119ed2d73f1c86600cfb91be0d14ea95cf
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-07-12 18:46:43 -0500

    Add dynamic updated date to blog page

--- siteConfig.mjs
+++ siteConfig.mjs
@@ -1,5 +1,11 @@
 export const siteConfig = {
-    title: "Sam Feldstein",
-    description: "Sam Feldstein's personal website. Sam is a writer and web developer from Bondurant, IA.",
-    url: "https://samfeldstein.xyz"
-}
+  title: "Sam Feldstein",
+  description:
+    "Sam Feldstein's personal website. Sam is a writer and web developer from Bondurant, IA.",
+  url: "https://samfeldstein.xyz",
+  location: {
+    country: "USA",
+    state: "LA",
+    city: "New Orleans",
+  },
+};
