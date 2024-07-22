4a45f5268c501df1a2c7183053fcffdbd15fa251
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-07-22 14:06:03 -0500

    Clean up tag page layout

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,3 +3,17 @@ export function sortCollectionByDate(collection) {
   collection.sort((a, b) => new Date(b.data.pubDate) - new Date(a.data.pubDate));
   return collection;
 }
+
+// Get collection tags
+export async function getStaticPaths(collection) {
+  const items = await getCollection(collection);
+  const uniqueTags = [...new Set(items.map((item) => item.data.tags).flat())];
+
+  return uniqueTags.map((tag) => {
+    const filteredPosts = items.filter((item) => item.data.tags.includes(tag));
+    return {
+      params: { tag },
+      props: { items: filteredPosts },
+    };
+  });
+}
