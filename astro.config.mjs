7add2ed9086d0b608e6564d482a8057cdb0f316d
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-07-23 10:15:13 -0500

    Add mdx and mess with picture element

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://samfeldstein.xyz',
-    prefetch: {
-      prefetchAll: true
-    },
-    vite: {
-        css: {
-          transformer: "sass",
-        },
-      },
+  site: 'https://samfeldstein.xyz',
+  prefetch: {
+    prefetchAll: true
+  },
+  vite: {
+    css: {
+      transformer: "sass"
+    }
+  },
+  integrations: [mdx()]
 });
