a5f3e4f691c9f7b777badb44838305670f5bf3f4
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-08-24 13:12:54 -0500

    Rewrite rss feed
    Apostrophes in titles rendering like this: &apos. I set encoding to only affect the post body, though I thought I'd already done that so I'm not sure this will make a difference.

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,32 +1,36 @@
 // https://docs.astro.build/en/guides/rss/
+
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { site } from "../../config.mjs";
 import { sortCollectionByDate } from "../scripts/utils";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
+
 const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection("blog");
   sortCollectionByDate(posts);
+
   return rss({
-    title: `${site.title}`,
-    description: `${site.description}`,
-    site: `${site.url}`,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      categories: post.data.tags,
-      customData: post.data.customData,
-      link: `/blog/${post.slug}/`,
-      // Note: this will not process components or JSX expressions in MDX files.
-      content: sanitizeHtml(parser.render(post.body), {
+    title: site.title,
+    description: site.description,
+    site: site.url,
+    items: posts.map((post) => {
+      const sanitizedContent = sanitizeHtml(parser.render(post.body), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-      }),
-      ...post.data,
-    })),
+      });
+
+      return {
+        title: post.data.title, // No sanitization needed here
+        pubDate: post.data.pubDate,
+        description: post.data.description, // Ensure this is plain text or sanitized if it includes HTML
+        categories: post.data.tags,
+        link: `/blog/${post.slug}/`,
+        content: sanitizedContent, // Only content is sanitized
+      };
+    }),
     customData: `<language>en-us</language>`,
   });
 }
