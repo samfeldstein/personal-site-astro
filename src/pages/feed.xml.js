0de96a08859ff686110e2e68f41713406c155c4d
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-08-24 12:09:04 -0500

    Add post content to feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -3,20 +3,28 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { site } from "../../config.mjs";
 import { sortCollectionByDate } from "../scripts/utils";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection("blog");
   sortCollectionByDate(posts);
   return rss({
     title: `${site.title}`,
-    description:
-      `${site.description}`,
+    description: `${site.description}`,
     site: `${site.url}`,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
+      customData: post.data.customData,
       link: `/blog/${post.slug}/`,
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+      ...post.data,
     })),
     customData: `<language>en-us</language>`,
   });
--- src/pages/site-roadmap.md
+++ src/pages/site-roadmap.md
@@ -6,6 +6,8 @@ title: Site Roadmap
 ## Site
 
 - [POSSE](https://indieweb.org/POSSE)
+  - Most streamlined way to do this maybe to set up a micro.blog site, then use API to send posts from here to there. Then let micro.blog handle crossposting. And if you use that, you may as well use it's newsletter capabilities anyway.
+  - Does Substack have an API?
 - Projects page
 - Photos page
 
