66340b61f57a318a42076c9e151a7a4aff95ce52
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-12-17 12:50:05 -0700

    Add photographer to site description

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 export const site = {
   title: "Sam Feldstein",
   description:
-    "Sam Feldstein's personal website. Sam is a writer and web developer from Bondurant, IA.",
+    "Sam Feldstein's personal website. Sam is a photographer, writer, and web developer from Bondurant, IA.",
   url: "https://samfeldstein.xyz",
   location: {
     country: "USA",
