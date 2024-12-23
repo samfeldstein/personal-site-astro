b19d7be03f9bba892ac8721c4fafef9fc74b3842
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-12-22 22:26:22 -0700

    Update site description

--- config.mjs
+++ config.mjs
@@ -1,7 +1,7 @@
 export const site = {
   title: "Sam Feldstein",
   description:
-    "Sam Feldstein's personal website. Sam is a web developer and photographer in Boulder, CO.",
+    "Sam Feldstein's personal website. Sam is a photographer and web developer in Boulder, CO. As a photographer, he focusses on portraits, concerts, and equestrian events. As a web developer, he focusses on custom websites for creatives, freelancers, and small businesses.",
   url: "https://samfeldstein.xyz",
   location: {
     country: "USA",
--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -4,17 +4,6 @@ export function sortCollectionByDate(collection) {
   return collection;
 }
 
-// export async function getGalleryImages() {
-//   const images = import.meta.glob('/src/images/gallery/**/*.{jpg,jpeg,png,webp}');
-
-//   // 3. Images are promises, so we need to resolve the glob promises
-//   const resolvedImages = await Promise.all(
-//     Object.values(images).map((image) => image().then((mod) => mod.default))
-//   );
-
-//   return resolvedImages;
-// }
-
 
 export function getGalleryImages() {
   // Eagerly load images for Astro's Picture component
