e46902d195e2539fdc47c816f46c46a17d06f8f4
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-01-05 15:36:35 -0700

    Simplify post layout
    Instead of it's own layout, just use [id].

---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+// https://docs.astro.build/en/tutorials/add-content-collections/#create-a-collection-for-your-blog-posts
+
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+import { glob } from 'astro/loaders';
+
+
+
+// Collections
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    location: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    draft: z.boolean().optional().default(false),
+  }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };
