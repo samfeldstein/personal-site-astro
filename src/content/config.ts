5a07ab4fec9dd14fa9aa71796e6cdde3015a8594
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-01-05 14:42:14 -0700

    Add digest post

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,21 @@
 // https://docs.astro.build/en/tutorials/add-content-collections/#create-a-collection-for-your-blog-posts
+
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
-// Define a `type` and `schema` for each collection
+
+// Collections
 const blogPosts = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    location: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()),
-    draft: z.boolean().optional().default(false),
-  }),
+  // schema: z.object({
+  //   title: z.string(),
+  //   pubDate: z.date(),
+  //   location: z.string(),
+  //   description: z.string(),
+  //   tags: z.array(z.string()),
+  //   draft: z.boolean().optional().default(false),
+  // }),
 });
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   blog: blogPosts,
