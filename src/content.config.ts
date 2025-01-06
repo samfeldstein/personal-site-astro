0b5bb377c40431a9c188045c2493e5450d29099c
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-01-05 17:57:52 -0700

    Add digests template!

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,15 +8,15 @@ import { glob } from 'astro/loaders';
 
 // Collections
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    location: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()),
-    draft: z.boolean().optional().default(false),
-  }),
+  loader: glob({ pattern: '**/*.{md,mdx,yml}', base: './src/content/blog' }),
+  // schema: z.object({
+  //   title: z.string(),
+  //   pubDate: z.date(),
+  //   location: z.string(),
+  //   description: z.string(),
+  //   tags: z.array(z.string()),
+  //   draft: z.boolean().optional().default(false),
+  // }),
 });
 
 // Export a single `collections` object to register your collection(s)
