5dc93da90631d33e5f20e924d407e0526b7a79f8
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-07-23 13:34:08 -0600

    Add quotes

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -5,7 +5,7 @@ const quotes = [
     work: "Justified",
   },
   {
-    quote: "The dude abides.",
+    quote: "The Dude abides.",
     speaker: "The Dude",
     work: "The Big Lebowski"
   },
@@ -20,13 +20,93 @@ const quotes = [
     work: "The Searchers"
   },
   {
-    quote: "Right now there's sorrow, pain. Don't kill it, and with it, all the joy you've felt.",
+    quote: `We rip out so much of ourselves to be cured of things faster that we go bankrupt by the age of thirty and have less to offer each time we start with someone new. But to make yourself feel nothing so as not to feel anything—what a waste! 
+      <br>
+      <br>
+      Right now there's sorrow, pain. Don't kill it, and with it, all the joy you've felt.`,
     speaker: "Mr. Perlman",
-    work: "Call Me By Your Name"
+    work: "Call Me By Your Name",
   },
   {
     quote: "To wives, and sweethearts. May they never meet.",
+    speaker: "Captain Jack Aubrey",
     work: "Master and Commander: The Far Side of the World"
+  },
+  {
+    quote: `“I wish it need not have happened in my time,” said Frodo.
+    <br>
+    <br>
+    “So do I,” said Gandalf, “and so do all who live to see such times. But that is not for them to decide. All we have to decide is what to do with the time that is given us.”`,
+    speaker: "Frodo Baggins and Gandalf the Grey",
+    work: "The Fellowship of the Ring",
+  },
+  {
+    quote: "The teeth, we don't know. Signs from Hashem? Don't know. Helping others. Couldn't hurt.",
+    speaker: "Rabbi Nachtner",
+    work: "A Serious Man"
+  },
+  {
+    quote: "In the end we blew off the last three pubs and headed for the hills. I sat up there, blood on my knuckles, beer down my shirt, sick on my shoes, knowing in my heart life would never feel this good again. And you know what? It never did.",
+    speaker: "Gary King",
+    work: "The World’s End"
+  },
+  {
+    quote: "If it was never new and it never gets old, then it’s a folk song.",
+    speaker: "Llewyn Davis",
+    work: "Inside Llewyn Davis"
+  },
+  {
+    quote: "I do not entertain hypotheticals. The world as it is is vexing enough.",
+    speaker: "Col. Stonehill",
+    work: "True Grit",
+    year: 2010
+  },
+  {
+    quote: "Fill your hands, you son of a bitch!",
+    speaker: "Rooster Cogburn",
+    work: "True Grit",
+    year: 2010
+  },
+  {
+    quote: "I am Iron Man",
+    speaker: "Tony Stark",
+    work: "Iron Man"
+  },
+  {
+    quote: "I. Drink. Your. MILKSHAKE!",
+    speaker: "Daniel Plainview",
+    work: "There Will Be Blood"
+  },
+  {
+    quote: "When the legend becomes fact, print the legend.",
+    speaker: "Maxwell Scott",
+    work: "The Man Who Shot Liberty Valance"
+  },
+  {
+    quote: "Thrill Me",
+    speaker: "Detective Ray Cameron",
+    work: "Night of the Creeps"
+  },
+  {
+    quote: `And should we win the day, the Fourth of July will no longer be known as an American holiday, but as the day when the world declared in one voice:
+    <br>
+    <br>
+    "We will not go quietly into the night! We will not vanish without a fight! We're going to live on! We're going to survive!"
+    <br>
+    <br>
+    Today, we celebrate our Independence Day!`,
+    speaker: "President Thomas J. Whitmore",
+    work: "Independence Day",
+  },
+  {
+    quote: "It’s okay with me.",
+    speaker: "Philip Marlowe",
+    work: "The Long Goodbye"
+  },
+  {
+    quote: "Get busy livin, or get busy dyin.",
+    speaker: "Red",
+    work: "Rita Hayworth and the Shawshank Redemption"
   }
 ]
 
