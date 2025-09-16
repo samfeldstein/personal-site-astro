d6119b4a2074f2f1052e4cd642ffab0a8bbb29e0
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-09-16 11:56:12 -0600

    Add print resume

--- src/data/resume.js
+++ src/data/resume.js
@@ -2,7 +2,7 @@ export const education = {
   chapman: {
     degree: "MFA, Screenwriting, withdrew",
     school: "Chapman University Dodge College of Film and Media Arts",
-    achievements: ["Panelist on the Incoming Student Welcoming Committee", "Teacher’s Assistant for Script Analysis"],
+    achievements: ["Panelist on the New Student Welcoming Committee", "Teacher’s Assistant for Script Analysis"],
     city: "Orange, CA",
     date: "December 2018",
   },
