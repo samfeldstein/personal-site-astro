da889e2f13db6bbd292a440bb3f6a0a2af37d5cd
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-10-25 10:18:17 -0600

    Add quote

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,5 +1,9 @@
 const quotes = [
   {
+    quote: "The trouble with the world is that the stupid are cocksure and the intelligent are full of doubt.",
+    speaker: "Bertrand Russell"
+  },
+  {
     quote: "How do you make God laugh? Tell him your plans.",
     speaker: "Yiddish Proverb"
   },
